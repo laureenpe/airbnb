41771fcb05437a59f3d7229603d653554b68ba2c
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-07 16:21:18 -0400

    update mac folder and json array working

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,19 @@ $(document).ready(function () {
 
 function loadFiles() {
     var html = "";
-    res.search_results.forEach(function (el) {
-        html += ` 
-                        <div class="col s6">
-                            <img id="photo`+ el.idimage + `" src='` + el.listing.thumbnail_url + `' >
-                        </div>
-                         <div class="col s6">
-                            <img id="photo`+ el.idimage + `" src='` + el.listing.thumbnail_url + `' >
 
-                        </div>
-                       
-                        
+    res.search_results.forEach(function (el) {
+        console.log(el.listing.reviews_count);
+        html += `  <div class="col s4">
+                         <img  id="photo`+ el.id + `"` + `" src="` + el.listing.thumbnail_url + `">
+                         
+                         <p class="description">`+ "$"+ " " + el.listing.cost + " " +"CLP" + " " +  el.listing.room_type + " "  +" "+ el.listing.localized_city + " "  + ` </p> 
+                         <p class="description">`+ el.listing.room_type + ` </p> 
+                         <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + ` </p>                          
+                    </div>
+
+                   
+                    
                     `
 
 

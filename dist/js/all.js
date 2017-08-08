28b42ddc1a77c545df958ca03c4bc119a5a9e034
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-08 11:54:24 -0400

    update profile pictures

--- dist/js/all.js
+++ dist/js/all.js
@@ -20270,17 +20270,20 @@ function loadFiles() {
     var html = "";
 
     res.search_results.forEach(function (el) {
-        console.log(el.listing.reviews_count);
+        console.log(el.listing.user.picture_url);
         html += `  
                     <div class= "col s12 m6 l6" >
                             <div class= "slide">
-                                <img class="responsive-img" id="photo`+ el.id + `"` + `" src="` + el.listing.thumbnail_url + `"
-                                    <i class="fa fa-heart" aria-hidden="true"></i>                        
-                            </div>        
+                                <img class="" id="photo`+ el.id + `"` + `" src="` + el.listing.thumbnail_url + `">
+                            </div> 
+                                <div class= "container-profile" >
+                                    <img class="circl" src="` + el.listing.user.picture_url+ `">
+                                 </div>        
+
                             <div class= "text">
                                 <p class="description">`+ "$" + " " + el.listing.cost + " " + "CLP" + " " + el.listing.room_type + " " + " " + el.listing.localized_city + " " + ` </p> 
                                 <p class="description">`+ el.listing.room_type + ` </p> 
-                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + ` </p>                  
+                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " "+  "Evaluaciones"+` </p>                  
                                 
                             </div>
 
@@ -20292,7 +20295,7 @@ function loadFiles() {
     })
 }
 
-
+//Function to filters
 var from_$input = $('#input_from').pickadate(),
     from_picker = from_$input.pickadate('picker')
 

769bfc4488132f82763ca4438c1c8c046e445369
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-09 10:29:00 -0400

    updated footer, index working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     loadFiles();
+    loadFilesIndex();
     $('select').material_select();
 
 });
@@ -10,7 +11,6 @@ $(document).ready(function () {
 
 function loadFiles() {
     var html = "";
-
     res.search_results.forEach(function (el) {
         console.log(el);
         html += `  
@@ -19,22 +19,48 @@ function loadFiles() {
                                 <img class="" id="photo`+ el.id + `"` + `" src="` + el.listing.thumbnail_url + `">
                             </div> 
                                 <div class= "container-profile" >
-                                    <img class="circl" src="` + el.listing.user.picture_url+ `">
+                                    <img class="circl" src="` + el.listing.user.picture_url + `">
                                  </div>        
 
                             <div class= "text">
                                 <p class="description">`+ "$" + " " + el.listing.cost + " " + "CLP" + " " + el.listing.property_type + " " + " " + el.listing.localized_city + " " + ` </p> 
                                 <p class="description">`+ el.listing.room_type + ` </p> 
-                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " "+  "Evaluaciones"+` </p>                  
+                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " " + "Evaluaciones" + ` </p>                  
                                 
                             </div>
 
                     </div>`
 
-
         $('.pictures').html(html);
+        $('.experience').html(html);
+
+
 
     })
 }
 
-//Function to filters
+//Function to index
+function loadFilesIndex() {
+    var html = "";
+    res.search_results.forEach(function (el) {
+        console.log(el);
+        html += `  
+                    <div class= "col s12 m6 l6 margin-image" >
+                            <div class= "slide">
+                                <img class="exper" id="photo`+ el.id + `` + `" src="` + el.listing.thumbnail_url + `">
+                            </div> 
+                                     
+                            <div class= "texto">
+                                <p class="description">`+ "$" + " " + el.listing.cost + " " + "CLP" + " " + el.listing.property_type + " " + " " + el.listing.localized_city + " " + ` </p> 
+                                <p class="descriptions">`+ el.listing.room_type + ` </p> 
+                                <p class="descriptions"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " " + "Evaluaciones" + ` </p>                            
+                            </div>
+
+                    </div>`
+
+        $('.experience').html(html);
+
+
+
+    })
+}
--- dist/js/all.js
+++ dist/js/all.js
@@ -20258,6 +20258,7 @@ var res = {"search_results":[{"listing":{"cost":6500,"bathrooms":1.0,"bedrooms":
 
 $(document).ready(function () {
     loadFiles();
+    loadFilesIndex();
     $('select').material_select();
 
 });
@@ -20268,7 +20269,6 @@ $(document).ready(function () {
 
 function loadFiles() {
     var html = "";
-
     res.search_results.forEach(function (el) {
         console.log(el);
         html += `  
@@ -20277,25 +20277,51 @@ function loadFiles() {
                                 <img class="" id="photo`+ el.id + `"` + `" src="` + el.listing.thumbnail_url + `">
                             </div> 
                                 <div class= "container-profile" >
-                                    <img class="circl" src="` + el.listing.user.picture_url+ `">
+                                    <img class="circl" src="` + el.listing.user.picture_url + `">
                                  </div>        
 
                             <div class= "text">
                                 <p class="description">`+ "$" + " " + el.listing.cost + " " + "CLP" + " " + el.listing.property_type + " " + " " + el.listing.localized_city + " " + ` </p> 
                                 <p class="description">`+ el.listing.room_type + ` </p> 
-                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " "+  "Evaluaciones"+` </p>                  
+                                <p class="description"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " " + "Evaluaciones" + ` </p>                  
                                 
                             </div>
 
                     </div>`
 
-
         $('.pictures').html(html);
+        $('.experience').html(html);
+
+
 
     })
 }
 
-//Function to filters
+//Function to index
+function loadFilesIndex() {
+    var html = "";
+    res.search_results.forEach(function (el) {
+        console.log(el);
+        html += `  
+                    <div class= "col s12 m6 l6 margin-image" >
+                            <div class= "slide">
+                                <img class="exper" id="photo`+ el.id + `` + `" src="` + el.listing.thumbnail_url + `">
+                            </div> 
+                                     
+                            <div class= "texto">
+                                <p class="description">`+ "$" + " " + el.listing.cost + " " + "CLP" + " " + el.listing.property_type + " " + " " + el.listing.localized_city + " " + ` </p> 
+                                <p class="descriptions">`+ el.listing.room_type + ` </p> 
+                                <p class="descriptions"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"        aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>`+ el.listing.reviews_count + " " + "Evaluaciones" + ` </p>                            
+                            </div>
+
+                    </div>`
+
+        $('.experience').html(html);
+
+
+
+    })
+}
 var map, places, infoWindow;
       var markers = [];
       var autocomplete;
